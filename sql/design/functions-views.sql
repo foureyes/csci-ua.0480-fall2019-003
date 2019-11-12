5f251491cdcb4e7c822ae3d17a69ed2f272b6379
Joe Versoza <jjv222@nyu.edu>
2019-11-12 17:34:34 -0500

    sql files for functions and views class based on practical sql

--- sql/design/functions-views.sql
+++ sql/design/functions-views.sql
@@ -1,6 +1,11 @@
-select title from staging_movie;
-
-
+-- Functions and Views, from our book -- Practical SQL by Anthony DeBarros
+--
+-- 0. create and import 2000 and 2010 data
+--    queries sourced directly from chapter 4, 6, and 16 of our book...
+--    see the associated github repo: https://github.com/anthonydb/practical-sql
+--    download the following csvs:
+--    https://github.com/anthonydb/practical-sql/blob/master/Chapter_04/us_counties_2010.csv
+--    https://github.com/anthonydb/practical-sql/blob/master/Chapter_06/us_counties_2000.csv
 CREATE TABLE us_counties_2010
 (
     geo_name                       varchar(90),    -- Name of the geography
@@ -107,13 +112,12 @@ CREATE TABLE us_counties_2010
     h0010003                       integer         -- Vacant
 );
 
-
-
+-- replace w/ location of your file
 COPY us_counties_2010
-FROM '/home/joeversoza/Downloads/us_counties_2010.csv.txt'
+FROM '/tmp/us_counties_2010.csv'
 WITH (FORMAT CSV, HEADER);
 
-
+select * from us_counties_2010;
 CREATE TABLE us_counties_2000 (
     geo_name varchar(90),              -- County/state name,
     state_us_abbreviation varchar(2),  -- State/U.S. abbreviation
@@ -133,15 +137,138 @@ CREATE TABLE us_counties_2000 (
     p0020003 integer                   -- Not Hispanic or Latino:
 );
 
-
+-- replace w/ location of your file
 COPY us_counties_2000
-FROM '/home/joeversoza/Downloads/us_counties_2000.csv.txt'
+FROM '/tmp/us_counties_2000.csv'
 WITH (FORMAT CSV, HEADER);
 
+-- a quick test
+select * from us_counties_2000 limit 50;
+select * from us_counties_2010 limit 50;
 
-select geo_name, state_us_abbreviation from us_counties_2000
-where geo_name ilike '%butler%';
+-- 1. join census data first
+--    use fips county and state to uniquely identify each row
+select c2010.geo_name, c2010.state_us_abbreviation, c2000.p0010001 as pop2000, c2010.p0010001 as pop2010,
+from us_counties_2000 c2000
+inner join us_counties_2010 c2010
+    on c2000.county_fips = c2010.county_fips
+    and c2000.state_fips = c2010.state_fips;
 
+-- 2. calculate pct_change and add to select list
+--    to avoid integer division, cast one operand to decimal
+select c2010.geo_name, c2010.state_us_abbreviation, c2000.p0010001 as pop2000, c2010.p0010001 as pop2010,
+       round((c2010.p0010001 -  c2000.p0010001)::decimal / c2000.p0010001, 2) as pct_change
+from us_counties_2000 c2000
+inner join us_counties_2010 c2010
+    on c2000.county_fips = c2010.county_fips
+    and c2000.state_fips = c2010.state_fips;
+
+
+-- 3. that was marginally annoying...
+--    let's write a function to doooo this
+--    functions can be written in plain SQL, plpgsql... and even Python, C, etc.
+--    if using SQL, function body (a string) is a series of SQL statements ending with ;.
+--    the result is either:
+--    * all rows fo the last query (if returning setof tablename)
+--    * the first row of the last query
+--
+--    ...let's try writing a function!
+--    a. use create or replace function so that you can continually redefine your function
+--    b. specify function signature:
+--       function_name(param1 type1, param2 type2, ... paramN, typeN)
+--    c. specify return value:
+--       returns type as
+--    d. add function body, a string literal... commonly delimited with $$ to allow string
+--       literals in function body
+--    e. specify what language is being used
+--       language sql
+create or replace function pct_change(old_val numeric, new_val numeric)
+returns decimal as
+    'select (new_val - old_val)::decimal / old_val;'
+language sql
+select pct_change(100, 150);
+-- rewrite our query
+select c2010.geo_name, c2010.state_us_abbreviation, c2000.p0010001 as pop2000, c2010.p0010001 as pop2010,
+       pct_change(c2000.p0010001,c2010.p0010001) as pct_change
+from us_counties_2000 c2000
+         inner join us_counties_2010 c2010
+                    on c2000.county_fips = c2010.county_fips
+                        and c2000.state_fips = c2010.state_fips
+-- 4. as an aside, it is possible to create a function that returns rows
+--    let's create a function that gets all rows from a state abbreviation
+--    make sure to use:
+--    returns setof us_counties_2010 as
+drop function get_by_state;
+create or replace function get_by_state(state_abbreviation varchar(2)) returns setof us_counties_2010 as
+'select * from us_counties_2010 where state_us_abbreviation = state_abbreviation;' language sql;
+-- test our function
+select * from get_by_state('NY');
+-- 5. back to our original function, you can use another language, plpgsql... it allows things like
+--    variables and loops (wat!? how are we so lucky?)
+--    NOTE.. THIS IS IN PLPGSQL!!!
+--    * declare variables in a declare block
+--    * use var_name type
+--    * uninitialized variables are set to null
+--    * can initialize with colon and equals.. :=
+--    * after declare, use transaction, begin end
+--    * assignment works with :=
+--    * alternative is query INTO var_name
+--    * can be used to assign
+--    * use return
+create or replace function pct_change(old_val numeric, new_val numeric) returns decimal as $$
+    declare
+        ret decimal;
+    begin
+        --ret := (new_val - old_val)::decimal / old_val;
+        select round((new_val - old_val)::decimal / old_val, 2) into ret;
+        return ret;
+    end;
+$$
+language plpgsql;
+select pct_change(6, 8);
+-- 6. ok, pct change works... back to our original query... if we want a table made out of it...
+--    an aside on view syntax (create or replace view view_name as query)
+create table webuser (
+    username varchar(255),
+    email varchar(255),
+    deleted bool default FALSE
+);
+insert into webuser values
+                           ('foo', 'foo@foo.foo'),
+                           ('bar', 'bar@bar.bar'),
+                           ('baz', 'baz@baz.baz'),
+                            ('qux', 'qux@qux.qux');
+update webuser set deleted = TRUE where username = 'foo';
+
+select * from webuser;
+select * from webuser where deleted = FALSE;
+
+create or replace view active_webuser as
+select * from webuser where deleted = FALSE;
+
+select * from active_webuser;
+--subquery one time use
+--CTE one time use, readability
+--function for procedures or calculations rather than subqueries
+-- * a series of inserts, updates, deletes
+-- * a query that is paramaterized - constrain funcionality
+-- * "domain specific language"
+-- views, multi use subquery has - still same as underlying table
+-- other tables, takes up space! needs updating - not same as underlying tables, tho
+create or replace view census_change as
+select c2010.geo_name, c2010.state_us_abbreviation, c2000.p0010001 as pop2000, c2010.p0010001 as pop2010,
+       pct_change(c2000.p0010001,c2010.p0010001) as pct_change
+from us_counties_2000 c2000
+         inner join us_counties_2010 c2010
+                    on c2000.county_fips = c2010.county_fips
+                        and c2000.state_fips = c2010.state_fips;
+select * from census_change;
+select * from census_change where state_us_abbreviation = 'NY';
+
+select count(distinct county_fips)
+from us_counties_2010 as c2010;
+
+-- other function examples
 create or replace function area_rect(length decimal, width decimal)
 returns numeric as
 'begin
